a6498dd6b4f4f18efe7f0e44cf391213943d940b
anurudra555 <72567564+anurudra555@users.noreply.github.com>
2021-09-18 21:06:20 +0530

    basic logic

--- app.js
+++ app.js
@@ -4,4 +4,20 @@ const currentPrice = document.querySelector("#current-price");
 const submitBtn = document.querySelector("#submit-btn");
 const outputDiv = document.querySelector("#output-div");
 
-console.log(initialPrice, stockQuantity, currentPrice, submitBtn, outputDiv);
+function calculateProfitAndLoss(initialPrice, stockQuantity, currentPrice) {
+    if(currentPrice > initialPrice) {
+        const profit = (currentPrice - initialPrice) * stockQuantity ;
+        const profitPercentage = (profit / initialPrice) * 100;
+        console.log(`Your profit on stocks is ${profit} & your profit percent is ${profitPercentage}`);
+    }
+    else if(initialPrice > currentPrice) {
+        const loss = (initialPrice - currentPrice) * stockQuantity;
+        const lossPercentage = (loss / initialPrice) * 100;
+        console.log(`Your loss on stocks is ${loss} & your loss percent is ${lossPercentage}`);
+    } 
+    else {
+        console.log("No pain no gain & No gain no pain");
+    }
+};
+
+calculateProfitAndLoss(100,20,1000);
