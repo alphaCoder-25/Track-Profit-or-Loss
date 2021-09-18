87096ed62ca197c83e5b17c9dc42ed3acbadf3f4
anurudra555 <72567564+anurudra555@users.noreply.github.com>
2021-09-18 20:30:38 +0530

    wiring done

---
+++ app.js
@@ -0,0 +1,7 @@
+const initialPrice = document.querySelector("#initial-price");
+const stockQuantity = document.querySelector("#stock-quantity");
+const currentPrice = document.querySelector("#current-price");
+const submitBtn = document.querySelector("#submit-btn");
+const outputDiv = document.querySelector("#output-div");
+
+console.log(initialPrice, stockQuantity, currentPrice, submitBtn, outputDiv);
